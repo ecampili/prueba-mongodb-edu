81b597b2cda11aca83bcff6d36fbeb557ead778b
ecampili <eduardo.campili@gmail.com>
2021-06-23 13:31:14 -0300

    se corrigio index

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
 
-app.listen(process.env.PORT, () => console.log(`Listening ar port: ${rocess.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`Listening ar port: ${process.env.PORT}`))
