4583598fc4f7de7221fb565ebffef6fd5eabdffc
ecampili <eduardo.campili@gmail.com>
2021-06-23 12:09:01 -0300

    se cambio el port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config()
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const { dbConnection } = require('./data/config')
 const cors = require('cors')
 
